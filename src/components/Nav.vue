430ec5c2f9b0382aae0ee384588d7efee0581bc8
hoipo <578344975@qq.com>
2021-07-04 18:49:08 +0800

    change the ico

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <div class="navbar-brand mb-0 h1">Bullbullgo</div>
+      <div class="navbar-brand mb-0 h1">Bullbullgo ｜ 为套利而生</div>
     </div>
   </nav>
 </template>
